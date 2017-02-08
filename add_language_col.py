61585b55c6b8880604c2a0985a386b11b39b1ea8
Simas <simas.tutlys@gmail.com>
2017-02-08 13:55:36 +0200

    Added progress report to stdout

--- add_language_col.py
+++ add_language_col.py
@@ -10,13 +10,19 @@ from __future__ import print_function
 from langdetect import detect
 import tablib
 import sys
+#import dataset later
 
 def main():    
     f=open(sys.argv[1],'rb')    
     data = data = tablib.Dataset().load(f.read())
     f.close()    
+    total = len(data)
     
-    lang = map(lambda tweet:detect(tweet),data['text'])
+    lang =[]
+    for i,tweet in enumerate(data['text']):
+        lang.append(detect(tweet))
+        print(i,"/",total,end="\r")
+        
     
     data.append_col(lang,header='lang')
     
@@ -28,11 +34,13 @@ def main():
     
     
 if __name__=="__main__":
+    formats=tablib.Dataset()._formats
+    inp = [fmt for fmt,funcs in formats.items() if funcs[1]!=None]
+    outp = [fmt for fmt in formats.keys() if (fmt not in inp)]    
     if (('-h' in sys.argv) or ("--help" in sys.argv)):
-        formats=tablib.Dataset()._formats
-        inp = [fmt for fmt,funcs in formats.items() if funcs[1]!=None]
-        outp = [fmt for fmt in formats.keys() if (fmt not in inp)]
+        
         print("Usage: python ",sys.argv[0]," input_file output_type\n")
+        print("Adds a language column (column name \'lang\') to tweet data.")
         print("If input and output types are the same,rewrites the original file\n")
         print("Supported input types: ", " ".join(inp))
         print("supported output types: "," ".join(formats.keys()))
