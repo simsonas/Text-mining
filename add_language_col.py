68c57496c8cf51294e79e10b2a69cc7a1f18f6a5
Simas <simas.tutlys@gmail.com>
2017-02-08 14:30:43 +0200

    comments,etikmat.

--- add_language_col.py
+++ add_language_col.py
@@ -14,21 +14,25 @@ import sys
 #import dataset later
 
 def main():    
+    #Open the file and read in the data    
     f=open(sys.argv[1],'rb')    
     data = data = tablib.Dataset().load(f.read())
     f.close()    
     total = len(data)
     
+    #language detection magic
     lang =[]
     for i,tweet in enumerate(data['text']):
         lang.append(detect(tweet))
         print(i,"/",total,end="\r")
         
-    
+    #append the language column
     data.append_col(lang,header='lang')
     
+    #split the filename by dot
     fname = sys.argv[1].split('.')
     
+    #save the output with output types' extension
     f = open('.'.join(fname[:-1])+"."+sys.argv[2],"wb")
     f.write(data.export(sys.argv[2]))
     f.close()
