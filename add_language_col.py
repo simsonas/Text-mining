12acc27b0d4a865ba7582490d2c0caec738f676d
Simas <simas.tutlys@gmail.com>
2017-02-12 16:36:18 +0200

    Preliminary .rda support

--- add_language_col.py
+++ add_language_col.py
@@ -11,21 +11,30 @@ from langdetect import detect
 import tablib
 import sys
 
-#import dataset later
+import dataset
+import pandas as pd
+import rpy2.robjects as robj
+from rpy2.robjects import pandas2ri
+
+pandas2ri.activate()
+
+def lang_det(data):
+    total = len(data)
+    
+    #language detection magic
+    lang =[]
+    for i,tweet in enumerate(data):
+        lang.append(detect(tweet))
+        print(i,"/",total,end="\r")
+    return lang
 
 def main():    
     #Open the file and read in the data    
     f=open(sys.argv[1],'rb')    
     data = data = tablib.Dataset().load(f.read())
     f.close()    
-    total = len(data)
     
-    #language detection magic
-    lang =[]
-    for i,tweet in enumerate(data['text']):
-        lang.append(detect(tweet))
-        print(i,"/",total,end="\r")
-        
+    lang = lang_det(data['text'])    
     #append the language column
     data.append_col(lang,header='lang')
     
@@ -36,19 +45,41 @@ def main():
     f = open('.'.join(fname[:-1])+"."+sys.argv[2],"wb")
     f.write(data.export(sys.argv[2]))
     f.close()
+
+def r_main():
+    robj.r['load'](sys.argv[1])
     
+    data = robj.r[sys.argv[2]]
+    
+    lang =lang_det(data.text)
+    
+    data['lang']=lang
+    
+    print(data)
+    
+    robj.globalenv[sys.argv[2]] = data
+
+    robj.r['save.image'](sys.argv[1])
+
     
 if __name__=="__main__":
     formats=tablib.Dataset()._formats
     inp = [fmt for fmt,funcs in formats.items() if funcs[1]!=None]
-    outp = [fmt for fmt in formats.keys() if (fmt not in inp)]    
+    outp = [fmt for fmt in formats.keys() if (fmt not in inp)]
+    inp.append("rda")
+    outp.append("rda")    
     if (('-h' in sys.argv) or ("--help" in sys.argv)):
         
-        print("Usage: python ",sys.argv[0]," input_file output_type\n")
+        print("Usage: python ",sys.argv[0]," {input_file output_type | rds_file dataframe}\n")
         print("Adds a language column (column name \'lang\') to tweet data.")
-        print("If input and output types are the same,rewrites the original file\n")
+        print("If input and output types are the same,rewrites the original file")
+        print("If the input file is .rda,we can only overwrite it.\n")
         print("Supported input types: ", " ".join(inp))
         print("supported output types: "," ".join(formats.keys()))
     else:
-        main()
+        fname = sys.argv[1].split('.')
+        if ("rda" in fname[-1].lower()):
+            r_main()
+        else:
+            main()
 
